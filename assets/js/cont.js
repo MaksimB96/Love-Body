fa1720249e4830b355955b4cf7c5d43138520b43
MaksimB96 <maksims.buraks.1996@gmail.com>
2022-12-09 13:33:22 +0000

    Add all pages and functional links

---
+++ assets/js/cont.js
@@ -0,0 +1,9 @@
+/**
+ * Function that allows burger menu to drop down and then close 
+ */
+
+function showMenu(){
+    document.querySelector('.navigate').classList.toggle('active');
+    document.querySelector('.menu').classList.toggle('hide');
+    document.querySelector('.close').classList.toggle('show');
+}
