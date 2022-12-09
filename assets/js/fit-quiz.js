94f8ce5da3f35c992f13d9ad20f866d38e3153c2
MaksimB96 <maksims.buraks.1996@gmail.com>
2022-12-09 17:00:46 +0000

    Add multiple new functions aand test questions to javascript

--- assets/js/fit-quiz.js
+++ assets/js/fit-quiz.js
@@ -7,3 +7,59 @@ function showMenu(){
     document.querySelector('.menu').classList.toggle('hide');
     document.querySelector('.close').classList.toggle('show');
 }
+
+let startButton = document.getElementById('start');
+let questionContElements = document.getElementById('question-cont');
+let questionElements = document.getElementById('question')
+
+let randomQuestion, currentQuestion;
+
+// question array and objects
+let questions = [
+    {
+        question: 'What does "RPE" stand for?',
+        answer: [
+            {text: 'Rate of Percieved Exhertion', correct: true},
+            {text: 'Rate of perspiration Experienced', correct: false},
+            {text: 'Rate of Percieved Exhertion', correct: true},
+            {text: 'Rate of Percieved Exhertion', correct: true},
+        ]
+    }
+]
+
+startButton.addEventListener('click', startGame);
+
+
+/**
+ * This function will intilize the game, add and remove class 'hide'
+ */
+function startGame(){
+    console.log('started');
+    startButton.classList.add('hide');
+    questionContElements.classList.remove('hide');
+
+    // questtions.sort will randomize my questions
+    randomQuestion = questions.sort(() => Math.random() - .5);
+    currentQuestion = 0;
+    nextQuestion()
+}
+
+/**
+ * This function will the next question to cycle through
+ */
+function nextQuestion(){
+    showQuestion(randomQuestion[currentQuestion])
+}
+
+/**
+ * 
+ */
+
+function showQuestion(question)
+
+/**
+ * This function will select user's answer
+ */
+function answerSelect(){
+
+}
