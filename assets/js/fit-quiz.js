c225c46c6b16d6c1b083d5aa9b0d9c9ecfa6ee71
MaksimB96 <maksims.buraks.1996@gmail.com>
2022-12-09 19:48:02 +0000

    Fix: typo errors that calls button with id next, fixed syntax error on set status class function

--- assets/js/fit-quiz.js
+++ assets/js/fit-quiz.js
@@ -12,24 +12,11 @@ let startButton = document.getElementById('start');
 let questionContElements = document.getElementById('question-cont');
 let questionElements = document.getElementById('question');
 let answerButtonElement = document.getElementById('answer-btn');
+let nextButton = document.getElementById('next');
 
-let randomQuestion, currentQuestion;
-
-// question array and objects
-let questions = [
-    {
-        question: 'What does "RPE" stand for?',
-        answer: [
-            {text: 'Rate of Percieved Exhertion', correct: true},
-            {text: 'Rate of perspiration Experienced', correct: false},
-            {text: 'Rate of Percieved Exhertion', correct: true},
-            {text: 'Rate of Percieved Exhertion', correct: true},
-        ]
-    }
-]
+let shuffledQuestion, currentQuestion;
 
 startButton.addEventListener('click', startGame);
-
 
 /**
  * This function will intilize the game, add and remove class 'hide'
@@ -40,16 +27,17 @@ function startGame(){
     questionContElements.classList.remove('hide');
 
     // questtions.sort will randomize my questions
-    randomQuestion = questions.sort(() => Math.random() - .5);
+    shuffledQuestion = questions.sort(() => Math.random() - .5);
     currentQuestion = 0;
-    nextQuestion()
+    nextQuestion();
 }
 
 /**
  * This function will the next question to cycle through
  */
 function nextQuestion(){
-    showQuestion(randomQuestion[currentQuestion])
+    resetMode();
+    showQuestion(shuffledQuestion[currentQuestion])
 }
 
 /**
@@ -58,11 +46,61 @@ function nextQuestion(){
 
 function showQuestion(question){
     questionElements.innerHTML = question.question;
+    question.answer.forEach(answer => {
+        let button =  document.createElement('button');
+        // inner text will be set to text in array
+        button.innerText = answer.text;
+        button.classList.add('btn');
+        // check if answer is correct
+        if(answer.correct){
+            button.dataset.correct = answer.correct
+        };
+        button.addEventListener('click', answerSelect);
+        answerButtonElement.appendChild(button);
+    })
+}
+
+function resetMode(){
+    nextButton.classList.add('hide');
+    while (answerButtonElement.firstChild) {
+        answerButtonElement.removeChild(answerButtonElement.firstChild);
+    };
 }
 
 /**
  * This function will select user's answer
  */
-function answerSelect(){
-
+function answerSelect(event){
+    let selectButton = event.target;
+    let correct = selectButton.dataset.correct;
+    setStatusClass(document.body, correct);
+    Array.from(answerButtonElement.children).forEach(button => {
+        setStatusClass(button, button.dataset.correct)
+    });
+    nextButton.classList.remove('hide');
 }
+
+function setStatusClass(element, correct){
+    clearStatusElement(element)  
+        if (correct){
+            element.classList.add('correct');
+        } else {
+            element.classList.add('wrong');
+        }
+}
+
+function clearStatusElement (element) {
+    element.classList.remove('correct');
+    element.classList.remove('wrong');
+}
+
+// question array and objects
+let questions = [
+    {
+        question: 'What does "RPE" stand for?',
+        answer: [
+            {text: 'Rate of Percieved Exhertion', correct: true},
+            {text: 'Rate of perspiration Experienced', correct: false},
+        ]
+    }
+]
