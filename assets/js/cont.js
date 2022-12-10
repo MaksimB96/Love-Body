1ac7b2ca7b3825c8652800b9da3b29a53c628817
MaksimB96 <maksims.buraks.1996@gmail.com>
2022-12-10 18:57:02 +0000

    Add emailjs functionality, by creating sendMail function that works on click

--- assets/js/cont.js
+++ assets/js/cont.js
@@ -7,3 +7,28 @@ function showMenu(){
     document.querySelector('.menu').classList.toggle('hide');
     document.querySelector('.close').classList.toggle('show');
 }
+/**
+ * This function will allow sstring values to be sent
+ */
+function sendMail(){
+    let sendInfo = {
+        name: document.getElementById('name').value ,
+        email: document.getElementById('email').value ,
+        message: document.getElementById('txtmsg').value
+    };
+    const serviceID = 'service_2m35b0r';
+    const templateID = 'template_fyhy87s';
+
+    emailjs.send(serviceID, templateID, sendInfo)
+    .then(
+    res =>{
+        document.getElementById('name').value = "";
+        document.getElementById('email').value = "";
+        document.getElementById('txtmsg').value = "";
+        console.log(res);
+        alert('Thanks for the feedback!')
+    })
+    .catch(err => console.log(err))
+    ;
+}
+
