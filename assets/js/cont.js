16990cd364431bd6be1ceeb7929c851ca3a97686
MaksimB96 <maksims.buraks.1996@gmail.com>
2022-12-11 19:38:56 +0000

    Add functions to determine if email entered in correctly and to apply set style

--- assets/js/cont.js
+++ assets/js/cont.js
@@ -18,14 +18,32 @@ form.addEventListener('submit', event => {
 });
 
 function validateForm() {
+    // trim used here to remove any white space an email may have 
     const emailValue = email.value.trim();
 
     if(emailValue === ''){
-        setError(email, 'Email is Required')
+        setError(email, 'Email is Required');
+    } else if {
+        setError(email, 'Provide a Valid Email');
+    } else {
+        setSuccess(email);
     }
 }
 
 
+function isEmailValid (email){
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+
+function setError(element, message){
+    const inputControl = element.parentElement;
+    const errorShow = inputControl.querySelector('.error');
+
+    errorShow.innerText = message;
+    inputControl.classList.add('error');
+    inputControl.classList.remove('success');
+}
 
 /**
  * This function will allow string values to be sent
