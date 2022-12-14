6951afbdb7dde0f09d43fa2e28f0384ed9cca1e0
MaksimB96 <maksims.buraks.1996@gmail.com>
2022-12-14 17:28:56 +0000

    Add styling and hover effect to screens 800px and above Remove: food quiz segment

--- assets/js/food-quiz.js
+++
@@ -1,9 +0,0 @@
-/**
- * Function that allows burger menu to drop down and then close 
- */
-
- function showMenu(){
-    document.querySelector('.navigate').classList.toggle('active');
-    document.querySelector('.menu').classList.toggle('hide');
-    document.querySelector('.close').classList.toggle('show');
-}
